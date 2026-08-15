bee41f8ce634935ee9b5e46f1d9136d48b7f27a5
Hetsoni28 <hetsony143@gmail.com>
2026-08-15 18:29:17 +0530

    chore(db): merge all migration heads and pull Manager/Viewer developer changes

---
+++ backend/app/db/migrations/versions/20260815_1257_92e91260fa5b_merge_all_heads.py
@@ -0,0 +1,24 @@
+"""merge_all_heads
+
+Revision ID: 92e91260fa5b
+Revises: 1cee6698e3fb, 0ce9ac4e6622
+Create Date: 2026-08-15 12:57:02.882057+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '92e91260fa5b'
+down_revision = ('1cee6698e3fb', '0ce9ac4e6622')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
