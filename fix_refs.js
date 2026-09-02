d3fe587dc3f7fb955fcdcecc590fb3a9a7daeb7e
Hetsoni28 <hetsony143@gmail.com>
2026-09-02 13:20:14 +0530

    chore: clean up temporary fix scripts

--- fix_refs.js
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-let file = fs.readFileSync("frontend/src/components/organisms/CopilotChat.tsx", "utf8");
-file = file.replace('disabled={isStreamingRef.current}', 'disabled={false}');
-file = file.replace('disabled={!input.trim() || isStreamingRef.current}', 'disabled={!input.trim()}');
-fs.writeFileSync("frontend/src/components/organisms/CopilotChat.tsx", file);
